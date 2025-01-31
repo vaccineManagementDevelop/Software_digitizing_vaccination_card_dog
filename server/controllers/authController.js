9bd1dbfaac188d656db6645f2c80b36d79bdb36a
Karina1014 <janneth_wi15@hotmail.com>
2025-01-31 09:37:17 -0500

    feat: :sparkles: implement user registration and login
    implement functionality register,login and logout

---
+++ server/controllers/authController.js
@@ -0,0 +1,120 @@
+import bcrypt from 'bcrypt';
+import userModel from '../models/userModel.js';
+
+
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.json({ success: false, message: 'Missing details' });
+  }
+
+  try {
+    const existingUser = await userModel.findOne({ email });
+
+    if (existingUser) {
+      return res.json({ success: false, message: 'User already exists' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new userModel({ name, email, password: hashedPassword });
+    await user.save();
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: '7d' }
+    );
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 días en milisegundos
+    });
+
+    //Sending welcome email
+    const mailOption = {
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject: 'Welcome to Karina',
+      text: `Welcome to Karina's website. Your account has been created with email ID: ${email}`
+    };
+    
+       await transporter.sendMail(mailOption);
+    //Sending welcome email fin
+
+    return res.json({ success: true });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: 'Something went wrong. Please try again later.' });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.json({
+      success: false,
+      message: 'Email and password are required',
+    });
+  }
+
+  try {
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({
+        success: false,
+        message: 'Invalid email',
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.json({ success: false, message: 'Invalid password' });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: '7d' }
+    );
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 días en milisegundos
+    });
+
+    return res.json({ success: true });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: 'Something went wrong. Please try again later.' });
+  }
+}
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie('token',{
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 
+            'none' : 'strict',
+          });
+        
+          return res.json({succes:true,
+            message:"Logged Out"
+          })
+
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false, message: 'Something went wrong. Please try again later.' });
+  
+    }
+
+  }
--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 import express from "express";
 import 'dotenv/config';
-import connectDB from "./config/mongodb.js"; 
-
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js"; // Asegúrate del nombre del archivo
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 connectDB();
+
+app.use(express.json());
+app.use(cookieParser());
+
+// API Endpoints
+app.get('/', (req, res) => res.send("API working"));
+app.use('/api/auth', authRouter);
 
 app.use((req, res) => {
     res.status(404).json({ success: false, message: 'Ruta no encontrada' });
