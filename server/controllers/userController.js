dd870907945327c89ade7c1ab07800086bdd19cd
Karina1014 <janneth_wi15@hotmail.com>
2025-01-31 11:55:32 -0500

    feat: :sparkles: add sendVerifiOTP
    add sendVerifiOTP function to send OTP for account verification

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -23,5 +23,38 @@ export const getUserData = async (req, res) => {
   }
 };
 
+export const sendVerifiOTP = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const user = await userModel.findById(userId);
 
+    // Verificar si la cuenta ya está verificada
+    if (user.isAccountVerified) {
+      return res.json({ success: false, message: "Account Already Verified" });
+    }
 
+    // Generar OTP
+    const otp = String(Math.floor(100000 + Math.random() * 900000));
+    user.verifyOtp = otp;  // Se cambió de sendVerifiOTP a verifyOtp para que sea consistente con la validación en verifiEmail
+    user.verifyOtpExpireAt = Date.now() + 24 * 60 * 60 * 1000; // Expira en 24 horas
+
+    // Guardar cambios en la base de datos
+    await user.save();
+
+    // Configuración del correo
+    const mailOption = {
+      from: process.env.SENDER_EMAIL,
+      to: user.email,
+      subject: 'Account Verification OTP',
+      text: `Your OTP is ${otp}. Verify your account using this OTP.`
+    };
+
+    // Enviar el correo
+    await transporter.sendMail(mailOption);
+
+    res.json({ success: true, message: "Verification OTP sent to email" });
+
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+}
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { register, login, logout} from '../controllers/authController.js';
+import { register, login, logout,sendVerifiOTP} from '../controllers/authController.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/register', register);
 authRouter.post('/login', login);
 authRouter.post('/logout', logout);
+authRouter.post('/send-verify-otp', userAuth, sendVerifiOTP);
 
 export default authRouter;
